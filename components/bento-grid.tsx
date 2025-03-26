1031f59bcd99184d52742cad90df730c96f36aad
jakku-git <jvkku@hotmail.com>
2025-03-26 16:07:43 +1100

    edits

--- components/bento-grid.tsx
+++ components/bento-grid.tsx
@@ -34,7 +34,7 @@ export function FeaturesSectionDemo() {
         "col-span-1 lg:col-span-3 lg:border-r  dark:border-neutral-800",
     },
     {
-      title: "Diversity is our strength",
+      title: "Strength In Diversity",
       description:
         "Our platform bridges Australia's local expertise with a vibrant Chinese market, sparking innovation and unlocking new growth opportunities for agents.",
       skeleton: <SkeletonFour />,
@@ -45,12 +45,11 @@ export function FeaturesSectionDemo() {
     <div className="relative z-20 py-10 lg:py-40 max-w-7xl mx-auto">
       <div className="px-8">
         <h4 className="text-3xl lg:text-5xl lg:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium text-black dark:text-white">
-          Packed with thousands of features
+          Trusted By Thousands of Agents
         </h4>
 
         <p className="text-sm lg:text-base  max-w-2xl  my-4 mx-auto text-neutral-500 text-center font-normal dark:text-neutral-300">
-          From Image generation to video generation, Everything AI has APIs for
-          literally everything. It can even create this website copy for you.
+          We are the leading Chinese real estate platform in Australia, providing comprehensive property listings and services to Chinese buyers and sellers.
         </p>
       </div>
 
